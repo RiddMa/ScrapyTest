b83a0d3a31127fad344c84a6ee0138a540b00503
RiddMa <riddma@qq.com>
2020-11-25 02:28:09 +0800

    PM25

--- PreProcessPM25/PM25.py
+++ PreProcessPM25/PM25.py
@@ -3,7 +3,7 @@ import time
 import pandas as pd
 
 # 1.打开CSV文件
-fileNameStr = 'BeijingPM20100101_20151231.csv'
+fileNameStr = './pm25-data-for-five-chinese-cities/BeijingPM20100101_20151231.csv'
 df = pd.read_csv(fileNameStr, encoding='utf-8')  # 不加dtype=str
 df.drop(df.columns[[range(10, 13)]], axis=1, inplace=True)
 df.drop(df.columns[[range(11, 15)]], axis=1, inplace=True)
@@ -16,15 +16,15 @@ df.to_csv("beijing.csv")
 start = time.time()
 df['sum'] = df[['PM_Dongsi', 'PM_Dongsihuan', 'PM_Nongzhanguan', 'PM_US Post']].sum(axis=1)
 df['count'] = df[['PM_Dongsi', 'PM_Dongsihuan', 'PM_Nongzhanguan', 'PM_US Post']].count(axis=1)
-df['ave'] = round(df['sum'] / df['count'], 2)
+df['avg'] = round(df['sum'] / df['count'], 2)
 end = time.time()
 
 # 5.输出到文件
-df.to_csv("beijing_ave.csv")
+df.to_csv("beijing_avg.csv")
 
 # 6.按照年做汇总，查看年的平均值
-print(df[['month', 'year', 'TEMP', 'ave']].groupby("year").mean())
-print(df[['month', 'year', 'TEMP', 'ave']].groupby('month').mean())
+print(df[['month', 'year', 'TEMP', 'avg']].groupby("year").mean())
+print(df[['month', 'year', 'TEMP', 'avg']].groupby('month').mean())
 for y in range(2010, 2016):
     print("year:" + str(y) + "月平均pm和气温")
     print(df.query('year=={}'.format(y)).groupby('month').mean())
